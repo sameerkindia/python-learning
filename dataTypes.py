db86f4728ebf7cf93178508fc751df166bfb6eec
sameerkindia <sameerkhanaa5@gmail.com>
2025-06-14 11:20:40 +0530

    python OOP

---
+++ dataTypes.py
@@ -0,0 +1,21 @@
+# Fundamental Data Types
+# int
+# float
+# bool
+# str
+# list
+# tuple
+# set
+# dict
+
+# Classes -> custom types
+
+# Specialized Data Types
+
+def sum(num1, num2):
+    print(num1 + num2)
+
+
+sum1 = sum(5, 10)
+
+print(sum1)
